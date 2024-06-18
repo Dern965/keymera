f29d5d8cc2db86ea23958b2f89ae5cb9888f8ce9
Dern965 <dern965@hotmail.com>
2024-06-18 12:59:51 -0600

    Script para borrar tablas de la base de datos

---
+++ Borrar_Tablas.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Configurar el cliente de DynamoDB local
+dynamodb_client = boto3.client('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2', aws_access_key_id='fakeMyKeyId', aws_secret_access_key='fakeSecretAccessKey')
+
+# Nombre de la tabla a borrar
+table_name = 'Ventas'
+
+# Borrar la tabla usando delete_table
+try:
+    response = dynamodb_client.delete_table(TableName=table_name)
+    print(f"Tabla '{table_name}' borrada exitosamente.")
+except Exception as e:
+    print("Error al borrar la tabla:", e)
